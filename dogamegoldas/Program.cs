2d93a02c0d90e08387d256681eeee5db01e32fc5
Marci02 <nagymarci.0218@gmail.com>
2022-11-14 08:08:02 +0100

    Add project files.

---
+++ dogamegoldas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace dogamegoldas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"adatok.txt\" fájl feldolgozása......");
+
+            StreamReader be = new StreamReader("adatok.txt");
+
+
+            be.Close();
+            Console.ReadKey();
+        }
+    }
+}
