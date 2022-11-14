61c46a95785768d573088773172a25487860917e
Marci02 <nagymarci.0218@gmail.com>
2022-11-14 08:30:42 +0100

    dogamegold

--- dogamegoldas/Program.cs
+++ dogamegoldas/Program.cs
@@ -15,8 +15,58 @@ namespace dogamegoldas
 
             StreamReader be = new StreamReader("adatok.txt");
 
+            int[] szamok = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                int szam = Convert.ToInt32(be.ReadLine());
+                szamok[i] = szam;
+            }
+
 
             be.Close();
+
+            // számok összegének
+
+            int szum = 0;
+
+            foreach (var sz in szamok)
+            {
+                szum = szum + sz;
+            }
+
+            Console.WriteLine($"Számok összege: {szum}");
+
+            // 5-el vagy 3-al osztható számok
+
+            Console.WriteLine("5-el vagy 3-al osztható számok:");
+
+            foreach (var sz in szamok)
+            {
+                if (sz % 5 == 0 || sz % 3 == 0)
+                {
+                    Console.WriteLine(sz);
+                }
+            }
+
+            Console.WriteLine("\n--**--**--\n");
+
+            Console.WriteLine("\"parban.txt\" fájl feldolgozása......");
+
+            StreamReader be2 = new StreamReader("parban.txt");
+
+            Console.WriteLine("Számok különbsége:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                string[] adatok = be2.ReadLine().Split(';');
+                int szam1 = Convert.ToInt32(adatok[0]);
+                int szam2 = Convert.ToInt32(adatok[1]);
+
+                Console.WriteLine(szam1 - szam2);
+            }
+
+            be2.Close();
             Console.ReadKey();
         }
     }
